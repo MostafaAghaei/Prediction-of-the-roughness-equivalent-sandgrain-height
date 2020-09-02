fd569e58b1a608fdda882952e20e8ff11b289b97
Mostafa Aghaei <aghaeijo@msu.edu>
2020-09-02 14:59:31 -0400

    matlab

--- 1/Cal_stat/surf_stat.m
+++ 1/Cal_stat/surf_stat.m
@@ -30,10 +30,48 @@ RMShei = sqrt( mean((hei1D - mean(hei1D)).^2));
 Shei = (mean((hei1D - mean(hei1D)).^3))/RMShei^3;
 Khei = (mean((hei1D - mean(hei1D)).^4))/RMShei^4;
 
+% ---------------  kt  ----------------------
+
+Lx=xc(end)-xc(1);
+Lz=zc(end)-zc(1);
+xleft=xc(1);
+zleft=zc(1);
+Lt=1;
+
+q=1;
+
+while (xleft<Lx)
+    while (zleft<Lz)
+        
+        if(Lx-xleft<Lt)
+            ix=ni;
+        else
+            ix=find(xc>=(xleft+Lt),1)-1;
+        end
+        
+        if(Lz-zleft<Lt)
+            iz=nk;
+        else
+            iz=find(zc>=(zleft+Lt),1)-1;
+        end
+        
+        xrgg=find(xc>=xleft,1):ix;
+        zrgg=find(zc>=zleft,1):iz;
+        Bbb(q)=max(max(hei(xrgg,zrgg)));
+        q=q+1;
+        zleft=zleft+Lt;
+    end
+    xleft=xleft+Lt;
+end
+
+k_t=sum(Bbb)/(q-1);
+
+clear Bbb
 
 
 fprintf('%%%% meanhei %5.5f\n',meanH)
 fprintf('%%%% maxhei %5.5f\n',maxH)
+fprintf('%%%% k_t %5.5f\n',k_t)
 fprintf('%%%% minhei %5.5f\n',minH)
 fprintf('%%%% k_rms = %3.5f\n',RMShei);
 fprintf('%%%% inc = %3.5f and %3.5f in x and z\n',incx,incz);
